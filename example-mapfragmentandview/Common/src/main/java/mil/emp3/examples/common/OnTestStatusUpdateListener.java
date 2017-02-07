b9351de17ae8a867d88ead6a449d0e81670304e3
Emp Barney <emp.barney@gmail.com>
2017-02-07 17:42:51 -0500

    Issue-1 #comment Removed get-bounds and Common. Issue-1 #comment Get-Bounds will be part of capabilities examples.

--- example-mapfragmentandview/Common/src/main/java/mil/emp3/examples/common/OnTestStatusUpdateListener.java
+++
@@ -1,9 +0,0 @@
-package mil.emp3.examples.common;
-
-import android.os.Handler;
-
-public interface OnTestStatusUpdateListener {
-    void onTestStatusUpdated(String updatedStatus);
-    void onTestCompleted(String completedTest);
-    Handler getHandler();
-}
