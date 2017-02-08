4ad7c59d7a9c8122141ca54b2db7a340172b6c6e
empbarney <emp.barney@gmail.com>
2017-02-08 11:34:48 -0500

    Feature/issue 1 add update remove (#2)
    * Issue-1 #comment Remove SingleMapView as it is not adding any value.
    
    * Issue-1 #comment Capabilities Tester brought over. Now we need to update
    the examples.
    
    * Issue-1 #comment Add remove Update Example added.
    
    * Issue-1 #comment Cleaned examples based on MapView and MapFragment that
    show how to display a map.
    
    * Issue-1 #comment Common and get-bounds are no longer used.
    
    * Issue-1 #comment Removed get-bounds and Common.
    Issue-1 #comment Get-Bounds will be part of capabilities examples.
    
    * Issue-1 #comment README merged into README.md
    
    * Update build.gradle
    
    * Issue-1 #comment Peer review comments addressed.

---
+++ example-capabilities/src/main/java/mil/emp3/examples/capabilities/containers/AddCircleFeature.java
@@ -0,0 +1,49 @@
+package mil.emp3.examples.capabilities.containers;
+
+import android.app.Activity;
+import android.util.Log;
+
+import java.util.List;
+
+import mil.emp3.api.Circle;
+import mil.emp3.api.interfaces.IMap;
+import mil.emp3.examples.capabilities.common.StyleManager;
+import mil.emp3.examples.capabilities.containers.dialogs.CirclePropertiesDialog;
+import mil.emp3.examples.capabilities.containers.dialogs.FeaturePropertiesDialog;
+import mil.emp3.examples.capabilities.dialogs.utils.ErrorDialog;
+
+public class AddCircleFeature extends AddEntityBase implements FeaturePropertiesDialog.FeaturePropertiesDialogListener<CirclePropertiesDialog> {
+    private static String TAG = AddCircleFeature.class.getSimpleName();
+
+    public AddCircleFeature(Activity activity, IMap map, IStatusListener statusListener, StyleManager styleManager) {
+        super(activity, map, statusListener, styleManager);
+    }
+
+    public String showPropertiesDialog(final List<String> parentList, final String featureName, final boolean visible) {
+        return showPropertiesDialog(CirclePropertiesDialog.newInstance("Circle Properties", map, parentList,
+                featureName, visible, AddCircleFeature.this), "fragment_circle_properties_dialog");
+    }
+
+    @Override
+    public boolean onFeaturePropertiesSaveClick(CirclePropertiesDialog dialog) {
+
+        final Circle newFeature;
+        try {
+            newFeature = new Circle();
+            newFeature.setRadius(dialog.getRadiusValue());
+            newFeature.setBuffer(dialog.getBufferValue());
+            addFeature(newFeature, dialog);
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG, "onFeaturePropertiesSaveClick", e);
+            statusListener.updateStatus(TAG, e.getMessage());
+            ErrorDialog.showError(activity, "Failed to add Feature " + e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public void onFeaturePropertiesCancelClick(CirclePropertiesDialog dialog) {
+
+    }
+}
--- example-mapfragmentandview/SampleMapView/src/main/java/mil/emp3/examples/samplemapview/MainActivity.java
+++ example-mapfragmentandview/SampleMapView/src/main/java/mil/emp3/examples/samplemapview/MainActivity.java
@@ -2,15 +2,17 @@ package mil.emp3.examples.samplemapview;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
 
 import mil.emp3.api.events.MapStateChangeEvent;
 import mil.emp3.api.exceptions.EMP_Exception;
 import mil.emp3.api.interfaces.IMap;
 import mil.emp3.api.listeners.IMapStateChangeEventListener;
-import mil.emp3.examples.common.CameraUtility;
+import mil.emp3.examples.maptestfragment.CameraUtility;
 import mil.emp3.examples.maptestfragment.MapFragmentAndViewActivity;
 
+/**
+ * Shows how MapView(s) can be added to the display via a layout configuration file.
+ */
 public class MainActivity extends MapFragmentAndViewActivity {
 
     private final static String TAG = MainActivity.class.getSimpleName();
@@ -19,9 +21,7 @@ public class MainActivity extends MapFragmentAndViewActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        testMenuFragment = ( mil.emp3.examples.maptestfragment.MapTestMenuFragment) getFragmentManager().findFragmentById(R.id.fooFragment);
 
-        testStatus = (TextView) findViewById(R.id.TestStatus);
         map = (IMap) findViewById(R.id.map);
         try {
             map.addMapStateChangeEventListener(new IMapStateChangeEventListener() {
@@ -31,6 +31,7 @@ public class MainActivity extends MapFragmentAndViewActivity {
                     try {
                         if(null == savedInstanceState) {
                             onMapReady(map);
+                            // Map shows West coast of US
                             map.setCamera(CameraUtility.buildCamera(33.9424368, -118.4081222, 2000000.0), false);
                         }
                     } catch (EMP_Exception e) {
@@ -51,7 +52,8 @@ public class MainActivity extends MapFragmentAndViewActivity {
                     try {
                         if(null == savedInstanceState) {
                             onMapReady(map2);
-                            map2.setCamera(CameraUtility.buildCamera(33.9424368, -118.4081222, 2000000.0), false);
+                            // Map shows East coast of US
+                            map2.setCamera(CameraUtility.buildCamera(40.7128, -74.0059, 2000000.0), false);
                         }
                     } catch (EMP_Exception e) {
                         e.printStackTrace();
