d1d6808b6c5181f73c595a237d6d30db2677a441
Miller <Matt.Miller@rgi-corp.com>
2017-08-31 15:40:34 -0400

    Revert "remove not working mapfragments"
    This reverts commit d0e915290bff55f42a33e085797042cdcc937e87.

---
+++ example-mapfragmentandview/SampleMapFragment/src/main/java/mil/emp3/examples/samplemapfragment/MyApplication.java
@@ -0,0 +1,11 @@
+package mil.emp3.examples.samplemapfragment;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
