5a3e9d331905cccb53378a6a045cd047d13f9959
Jenifer Cochran <jenifer.cochran@rgi-corp.com>
2017-11-07 08:18:31 -0500

    adding the ability to plot military symbology

--- example-kmz-exportimport/src/main/java/mil/emp3/example_kmz_exportimport/MainActivity.java
+++ example-kmz-exportimport/src/main/java/mil/emp3/example_kmz_exportimport/MainActivity.java
@@ -6,28 +6,54 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import org.cmapi.primitives.GeoPosition;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.UUID;
 
+import mil.emp3.api.Camera;
 import mil.emp3.api.Overlay;
-import mil.emp3.api.Point;
 import mil.emp3.api.exceptions.EMP_Exception;
+import mil.emp3.api.interfaces.ICamera;
 import mil.emp3.api.interfaces.IEmpExportToTypeCallBack;
+import mil.emp3.api.interfaces.IFeature;
 import mil.emp3.api.interfaces.IMap;
 import mil.emp3.api.utils.kmz.EmpKMZExporter;
 
 
 public class MainActivity extends AppCompatActivity
 {
-    private IMap map;
+    private IMap    map;
+    private ICamera oCamera = new Camera();
+    private Overlay overlay = new Overlay();
+    private HashMap<UUID, IFeature> oFeatureHash = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        map = (IMap) findViewById(R.id.map);
+        this.map = (IMap) findViewById(R.id.map);
+
+        //setup overlay
+        this.overlay.setName("Test Overlay");
+
+        try
+        {
+            this.map.setCamera(this.oCamera, false);
+            this.map.addOverlay(overlay, true);
+        }
+        catch (EMP_Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void plotMilitarySymbology(View view)
+    {
+        PlotUtility.plotManyMilStd(100, this.overlay, this.oFeatureHash, this.oCamera);
     }
 
 
@@ -37,22 +63,10 @@ public class MainActivity extends AppCompatActivity
         {
             return;
         }
-        Point             feature  = new Point();
-        final GeoPosition location = new GeoPosition();
-        Overlay           overlay  = new Overlay();
-
-        //setup overlay
-        overlay.setName("Test Overlay");
-
-        //setup feature
-        location.setLatitude(30.0);
-        location.setLongitude(-25.0);
-
-        feature.setPosition(location);
 
         try
         {
-            this.map.addOverlay(overlay, true);
+            this.map.addOverlay(this.overlay, true);
         }
         catch (EMP_Exception e)
         {
@@ -63,7 +77,7 @@ public class MainActivity extends AppCompatActivity
         tempDirectory.mkdirs();
 
         EmpKMZExporter.exportToKMZ(this.map,
-                                   overlay,
+                                   this.overlay,
                                    false,
                                    new IEmpExportToTypeCallBack<File>()
                                                                        {
@@ -71,11 +85,16 @@ public class MainActivity extends AppCompatActivity
                                                                            public void exportSuccess(final File exportObject)
                                                                            {
                                                                                //ExportObject is the kmz file
-                                                                               exportObject.exists();
+                                                                               Toast.makeText(MainActivity.this.getApplicationContext(),
+                                                                                             String.format("Export successful. Saved to %s",exportObject.getAbsolutePath()),
+                                                                                             Toast.LENGTH_LONG);
                                                                            }
                                                                            @Override
                                                                            public void exportFailed(final Exception Ex)
                                                                            {
+                                                                               Toast.makeText(MainActivity.this.getApplicationContext(),
+                                                                                              String.format("Export failed. %s",Ex.getMessage()),
+                                                                                              Toast.LENGTH_LONG);
                                                                            }
                                                                        },
                                    tempDirectory.getAbsolutePath(),
