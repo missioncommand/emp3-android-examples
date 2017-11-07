f7aeb2b493d24de718509f3c58f8bd788fb15926
Jenifer Cochran <jenifer.cochran@rgi-corp.com>
2017-11-07 08:54:42 -0500

    adding a kmz import example, still a WIP

--- example-kmz-exportimport/src/main/java/mil/emp3/example_kmz_exportimport/MainActivity.java
+++ example-kmz-exportimport/src/main/java/mil/emp3/example_kmz_exportimport/MainActivity.java
@@ -1,7 +1,9 @@
 package mil.emp3.example_kmz_exportimport;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,16 +13,26 @@ import android.widget.Toast;
 import org.cmapi.primitives.GeoPosition;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
 import java.util.HashMap;
 import java.util.UUID;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import mil.emp3.api.Camera;
+import mil.emp3.api.KMLS;
 import mil.emp3.api.Overlay;
+import mil.emp3.api.enums.KMLSEventEnum;
 import mil.emp3.api.exceptions.EMP_Exception;
 import mil.emp3.api.interfaces.ICamera;
 import mil.emp3.api.interfaces.IEmpExportToTypeCallBack;
 import mil.emp3.api.interfaces.IFeature;
 import mil.emp3.api.interfaces.IMap;
+import mil.emp3.api.interfaces.IMapService;
 import mil.emp3.api.utils.kmz.EmpKMZExporter;
 
 
@@ -56,10 +68,33 @@ public class MainActivity extends AppCompatActivity
         PlotUtility.plotManyMilStd(100, this.overlay, this.oFeatureHash, this.oCamera);
     }
 
+    public void importKmzExample(View view)
+    {
+        if(hasRequiredPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == false)
+        {
+            return;
+        }
+        File targetFile = FileUtility.getExampleKmzFile(getApplicationContext());
+        try
+        {
+            IMapService mapService = new KMLS(getApplicationContext(), targetFile.toURI().toURL().toString(), new KMLSServiceListener(new LinkedBlockingQueue<KMLSEventEnum>(), this.map));
+            mapService.setName("kmzSample_Test");
+            this.map.addMapService(mapService);
+        }
+        catch (EMP_Exception e)
+        {
+            e.printStackTrace();
+        }
+        catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 
     public void exportToKmzExample(View view)
     {
-        if(hasRequiredPermissions() == false)
+        if(hasRequiredPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == false)
         {
             return;
         }
@@ -95,19 +130,19 @@ public class MainActivity extends AppCompatActivity
 
     private static final int PERMISSION_REQUEST_CODE = 1;
 
-    private boolean hasRequiredPermissions()
+    private boolean hasRequiredPermission(String permission)
     {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M)
         {
 
-            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            if (checkSelfPermission(permission)
                     == PackageManager.PERMISSION_DENIED)
             {
 
                 Log.d("permission", "permission denied to Write External Storage - requesting it");
-                String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                String[] permissions = {permission};
 
-                requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+                requestPermissions(permissions, permission.hashCode());
                 return false;
             }
         }
